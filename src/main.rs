9d3e9507f0c06727aa90fda37f154054bec0f2fa
MutebiAlvin <mutebialvinalvienzo@gmail.com>
2023-08-04 09:38:26 +0300

    Add Capture User Input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        let mut input = String::new();
+        println!("Please Enter a geek word (or 'exit' to quit):");
+
+        io::stdin().read_line(&mut input).unwrap();
+        let input = input.trim();
+
+        if input == "exit" {
+            break;
+        }
+    }
 }
